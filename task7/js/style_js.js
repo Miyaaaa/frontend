9be5e7d792ea8967e7e975583a727b4eb3aecf8a
Miyaaaa <583871106@qq.com>
2016-07-24 23:22:25 +0800

    july 24th commit

---
+++ task7/js/style_js.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/7/24.
+ */
+$(function(){
+    var div4_pic1_width = $("#div4_pic1").width();
+    var div4_form_width = $("#div4_form").width();
+    $("#div4_form").css("left",(div4_pic1_width-div4_form_width)/2+"px");
+    //var div7_pic1_height = $("#div7_pic1").height();
+    //$(".div7_sub").css("height", div7_pic1_height+"px");
+});
