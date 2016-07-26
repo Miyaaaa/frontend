4b0922ea9d71365dfb74e6c23b1943aad1b27765
Miyaaaa <583871106@qq.com>
2016-07-27 00:20:59 +0800

    july 27th

--- task7/js/style_js.js
+++ task7/js/style_js.js
@@ -5,6 +5,4 @@ $(function(){
     var div4_pic1_width = $("#div4_pic1").width();
     var div4_form_width = $("#div4_form").width();
     $("#div4_form").css("left",(div4_pic1_width-div4_form_width)/2+"px");
-    //var div7_pic1_height = $("#div7_pic1").height();
-    //$(".div7_sub").css("height", div7_pic1_height+"px");
 });
